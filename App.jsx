de9c432de86cb415b94235456891c6adb10cc906
isabellamanfroi <isabella.manfroi@escola.pr.gov.br>
2025-08-25 09:57:17 -0300

    spotify

--- App.jsx
+++ App.jsx
@@ -1,1 +1,31 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Image} from 'react-native';
 
+export default function App(){
+    return (
+        <View style={styles.container}>
+            <Image
+                //Caminho da imagem
+                source={require('./assets/spotify-logo.png')}
+                style={styles.logo}
+                resizeMode="contain"
+                acessible
+                accessibilityLabel = "Spotify"
+                />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000', //Cor preta
+        alignItems: 'center',
+        justifyContent:'center'
+    },
+
+    logo:{
+        width: 100,
+        height: 100,
+    },
+});
